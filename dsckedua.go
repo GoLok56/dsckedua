4983407ae63586934a17292f2cc9d73395d3faff
Satria Adi Putra <satadii11@gmail.com>
2019-10-18 04:31:24 +0700

    2-Struct

--- dsckedua.go
+++ dsckedua.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Buku struct {
+	judul    string
+	penulis  string
+	penerbit string
+}
 
 func main() {
-	umurOrang := make(map[string] int)
-	umurOrang["James"] = 17
-	umurOrang["Catalunya"] = 18
+	var buku Buku
+	var scanner = bufio.NewReader(os.Stdin)
+	fmt.Println("Masukkan judul buku")
+	buku.judul, _ = scanner.ReadString('\n')
 
-	fmt.Println("Umurnya James adalah", umurOrang["James"])
+	fmt.Println("Masukkan penulis buku")
+	buku.penulis, _ = scanner.ReadString('\n')
+
+	fmt.Println("Masukkan penerbit buku")
+	buku.penerbit, _ = scanner.ReadString('\n')
+
+	fmt.Println("Judul:", buku.judul, "\nPenulis:", buku.penulis, "\nPenerbit:", buku.penerbit)
 }
